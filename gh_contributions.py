740f012f50ed38e092582018589b586405f0286c
Willmar Knikker <wil7vin@live.nl>
2017-10-11 21:11:39 +0200

    First commit

---
+++ gh_contributions.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+#
+# Print your contributions for the current day
+# Usage: ghb contributions
+#
+
+import signal
+import sys
+from datetime import datetime
+from html.parser import HTMLParser
+from requests import get
+
+URL = "https://github.com/users/%s/contributions"
+
+class CustomHTMLParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.rects = []
+
+    def handle_starttag(self, tag, attrs):
+        if tag == "rect":
+            self.rects.append(attrs)
+
+
+def signal_handle(sig, frame):
+    sys.exit(0)
+
+
+def pluralize(number):
+    if number == 1:
+        return ""
+    return "s"
+
+def get_count_date(username):
+    r = get(URL % username)
+    parser = CustomHTMLParser()
+    parser.feed(r.text)
+    d = dict(parser.rects[-1])
+    number = d["data-count"]
+    date = d["data-date"]
+    return number, date
+
+def get_current_streak(username):
+    r = get(URL % username)
+    parser = CustomHTMLParser()
+    parser.feed(r.text)
+    
+    rev_rects = list(reversed(parser.rects))
+    rev_rects.pop(0)
+
+    count = 0
+    for i in rev_rects:
+        d = dict(i)
+        if int(d["data-count"]) > 0:
+            count = count + 1
+        else:
+            break
+
+    return count
+
+def get_longest_steak(username):
+    r = get(URL % username)
+    parser = CustomHTMLParser()
+    parser.feed(r.text)
+
+    max_steak = 0
+    count = 0 
+    for i in reversed(parser.rects):
+        d = dict(i)
+        if int(d["data-count"]) > 0:
+            count = count + 1
+        else:
+            if count > max_steak:
+                max_streak = count
+                count = 0
+    
+    return max_streak
+
+def get_commited_today(username):
+    num, date = get_count_date(username)
+    num = int(num)
+    cur_date = datetime.now().strftime('%Y-%m-%d')
+    if date == cur_date:
+        if num > 0:
+            return True
+    return False
---
+++ gh_reminder.py
@@ -0,0 +1,84 @@
+import pgi
+import datetime
+import time
+import math
+import gh_contributions
+import threading
+from pgi.repository import Notify
+
+Notify.init('Github Steak Reminder')
+
+checked_hours = []
+commited_today = False
+
+def send_reminder(title, message, urgency):
+    n = Notify.Notification.new(title, message, 'dialog-information')
+    n.set_urgency(urgency)
+    n.show()
+
+
+def min_until_midnight():
+     now = datetime.datetime.now()
+     tomorrow = datetime.datetime(now.year, now.month, now.day) + \
+                datetime.timedelta(1)
+     return math.floor(abs(tomorrow - now).seconds / 60)
+
+def CheckContibutions():
+    global checked_hours
+    global commited_today
+
+    timestamp = datetime.datetime.now().time()
+
+    if timestamp.hour == 0:
+        checked_hours = []
+        commited_today = False
+
+    if (((timestamp.hour in checked_hours) and timestamp.hour != 23) or commited_today == True):
+        t = threading.Timer(60, CheckContibutions)
+        t.start()
+        return
+
+    checked_hours.append(timestamp.hour)
+
+    if gh_contributions.get_commited_today('DrNotThatEvil') == False:
+        minutes = min_until_midnight()
+        longest_s = gh_contributions.get_longest_steak("DrNotThatEvil")
+        current_s = gh_contributions.get_current_streak("DrNotThatEvil")
+        
+        if timestamp.hour == 23:
+            # Last hour of the day now lets remind the user every 15 minutes.
+            if ((timestamp.minute+1) % 15) == 0 and (timestamp.minute+1) < 45:
+                # fifteen minutes but not the last 45 minutes 
+                text = "You only have {} minutes left to maintain your GitHub.com steak\n"
+                text = text + "Your current streak is {} days.\nYour Longest streak is {} days"
+                text = text.format(current_s, longest_s)
+                send_reminder("Github steak Reminder", text, 1)
+
+            if ((timestamp.minute+1) > 50):
+                text = "IMPORTANT: You only have {} minutes! left to maintain your GitHub.com steak\n"
+                text = text + "Your current streak is {} days.\nYour Longest streak is {} days"
+                text = text.format(minutes, current_s, longest_s)
+                send_reminder("Github steak Reminder", text, 1)
+        else:
+
+            text = "You have {} minutes left to commit something or you lose your GitHub.com steak.\n\n" 
+            text = text + "Your current streak is: {} days.\nYour longest is {} days."
+            text = text.format(minutes, current_s, longest_s)
+
+            send_reminder("Github steak Reminder", text, 1)
+    else:
+        commited_today = True
+        commit_count, _ = gh_contributions.get_count_date('DrNotThatEvil')
+        text = "You have commited today! Here are your stats.\n\n" 
+        text = "Today's commit count: {}\n"
+        text = text + "Your current streak is: {} days.\nYour longest is {} days."
+        text = text.format(commit_count, minutes, current_s, longest_s)
+
+        
+    t = threading.Timer(60, CheckContibutions)
+    t.start()
+
+if __name__ == "__main__":
+    ##t = threading.Timer(1, CheckContibutions)
+    ##t.start()
+    CheckContibutions()
